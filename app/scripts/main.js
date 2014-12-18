99facb197c38033ebdc7655fc539f7f3b42e6529
Ashugeo <hugo.saintemarie@gmail.com>
2014-12-18 18:20:32 +0100

    Code with tabs, coloring and editing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,9 +6,9 @@ function updateMenuIndicator( sectionId ) {
         bbox = $menuItem[0].getBoundingClientRect(),
         middle = bbox.left + bbox.width / 2,
         totalWidth = $(document.body).width();
-    
+
     currentSectionId = sectionId;
-    
+
     $('#menu-indicator .left').css('width', middle );
     $('#menu-indicator .right').css('width', totalWidth - middle );
     $('.nav_active').removeClass();
@@ -16,18 +16,20 @@ function updateMenuIndicator( sectionId ) {
 }
 
 $(function(){
-    // set default color for a_home
+    // set defaults
     $('#a_home').addClass('nav_active');
-    
+    $('.result').addClass('label_active');
+    $( "#result" ).prop( "checked", true );
+
     // does menu shrink ?
     //if(($(window).width()) < 950){
     //    var headerHeight = 100;
     //} else {
     //    headerHeight = 100;
     //}
-    
+
     var speed=1000;
-    
+
     skrollr.init({
         keyframe: function(element, name, direction) {
             if (allowUpdateMenuIndicator){
@@ -35,7 +37,7 @@ $(function(){
             }
         }
     });
-    
+
     $(document).on('click', '#header a', function(){
 
         var idSectionCible = this.href.replace(/.*?(#.*)$/, '$1');
@@ -43,18 +45,36 @@ $(function(){
         allowUpdateMenuIndicator = false;
 
         $(document.body).stop().animate({
-            // TODO: 90 ne devrait pas être en dur
-            scrollTop: $(idSectionCible).offset().top - 90
+            // TODO: 70 ne devrait pas être en dur
+            scrollTop: $(idSectionCible).offset().top - 70
 
         }, 1000, function() {
             allowUpdateMenuIndicator = true;
         });
-        
+
         return false;
     });
-    
+
+    $(document).on('click', 'label', function(){
+        $(this).closest('ul').find('label').removeClass('label_active');
+        $(this).addClass('label_active');
+    });
+
     $(window).on('resize', function() {
         updateMenuIndicator(currentSectionId);
     });
-    
+
+
+//    $(document).on('click', '#source', function(){
+//        if ($("#source").text() == 'Source'){
+//            $("#source").text("Run");
+//            $(".CodeMirror").css('display', 'block');
+//        } else if ($("#source").text() == 'Run'){
+//            $("#source").text("Source");
+//            $(".CodeMirror").css('display', 'none');
+//        }
+//
+//        return false;
+//    });
+
 });
